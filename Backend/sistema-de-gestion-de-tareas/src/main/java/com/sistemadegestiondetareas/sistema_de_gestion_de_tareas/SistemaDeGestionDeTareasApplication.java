3385e962effec64e0c89bf9360907ab77a447b3c
jafeth gamboa <jafethgamboabaas@gmail.com>
2025-09-27 04:38:08 -0600

    Add global styles and layout for Next.js project with Tailwind CSS integration
    - Created globals.css for global styles including dark mode support
    - Implemented layout.js to set up font variables and metadata
    - Developed page.js with a structured layout, including navigation and content sections
    - Integrated images and links for deployment and documentation

---
+++ Backend/sistema-de-gestion-de-tareas/src/main/java/com/sistemadegestiondetareas/sistema_de_gestion_de_tareas/SistemaDeGestionDeTareasApplication.java
@@ -0,0 +1,13 @@
+package com.sistemadegestiondetareas.sistema_de_gestion_de_tareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestionDeTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGestionDeTareasApplication.class, args);
+	}
+
+}
---
+++ Backend/sistema-de-gestion-de-tareas/src/test/java/com/sistemadegestiondetareas/sistema_de_gestion_de_tareas/SistemaDeGestionDeTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemadegestiondetareas.sistema_de_gestion_de_tareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGestionDeTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
